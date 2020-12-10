84910c1ff306d2b567742ba35d98bd1793763a91
Ivan Gyoshev <61377588+ivangyoshev-cs@users.noreply.github.com>
2020-12-11 00:02:56 +0200

    Add files via upload

---
+++ C# DB-Advanced EF Core/Exams/C# DB Advanced Exam - 18 Apr 2019/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)][MaxLength(40)]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price { get; set; } =Songs.Sum(s => s.Price);        public int ProducerId { get; set; }
+        public Producer Producer { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; } = new HashSet<Song>();
+    }
+}
